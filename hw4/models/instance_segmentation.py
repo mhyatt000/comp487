f657896a3557a18c6d1cf959b9440f6f8adc56be
Matthew Hyatt <mhyatt000@gmail.com>
2022-04-05 22:19:41 -0500

    done

--- hw4/models/instance_segmentation.py
+++ hw4/models/instance_segmentation.py
@@ -1,9 +1,40 @@
+import functools
 import os
+import time
+from argparse import ArgumentParser
+
 import torch
 import torchvision
+from d2l import torch as d2l
 from torch import nn
 from torch.nn import functional as F
-from d2l import torch as d2l
+from torchsummary import summary
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+
+def get_args():
+
+    ap = ArgumentParser(description='args for hw4')
+
+    ap.add_argument('-s', '--save', action='store_true')
+    ap.add_argument('-l', '--load', action='store_true')
+    ap.add_argument('-t', '--train', action='store_true')
+    ap.add_argument('-v', '--verbose', action='store_true')
+    ap.add_argument('-e', '--epochs', type=int)
+
+    args = ap.parse_args()
+
+    if not args.epochs:
+        args.epochs = 5
+
+    return args
+
+class Environment():
+    'environment variables to be passed around'
+
+    def __init__(self):
+        pass
 
 
 def get_voc2012():
@@ -13,7 +44,20 @@ def get_voc2012():
     )
 
 
-voc_dir = d2l.download_extract("voc2012", "VOCdevkit/VOC2012")
+def get_voc_maps():
+
+    VOC_COLORMAP = [
+        [0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128], [128, 0, 128],
+        [0, 128, 128], [128, 128, 128], [64, 0, 0], [192, 0, 0], [64, 128, 0],
+        [192, 128, 0], [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
+        [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0], [0, 64, 128],
+    ]
+    VOC_CLASSES = [
+        "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car",
+        "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person",
+        "potted plant", "sheep", "sofa", "train", "tv/monitor"
+    ]
+    return VOC_COLORMAP, VOC_CLASSES
 
 
 def read_voc_images(voc_dir, is_train=True):
@@ -41,41 +85,8 @@ def read_voc_images(voc_dir, is_train=True):
     return features, labels
 
 
-train_features, train_labels = read_voc_images(voc_dir, True)
-
-n = 5
-imgs = train_features[0:n] + train_labels[0:n]
-imgs = [img.permute(1, 2, 0) for img in imgs]
-d2l.show_images(imgs, 2, n)
-
-VOC_COLORMAP = [
-    [0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128], [128, 0, 128],
-    [0, 128, 128], [128, 128, 128], [64, 0, 0], [192, 0, 0], [64, 128, 0],
-    [192, 128, 0], [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
-    [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0], [0, 64, 128],
-]
-
-VOC_CLASSES = [
-    "background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car",
-    "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person",
-    "potted plant", "sheep", "sofa", "train", "tv/monitor"
-]
-
-
-pretrained_net = torchvision.models.resnet18(pretrained=True)
-list(pretrained_net.children())[-3:]
-
-net = nn.Sequential(*list(pretrained_net.children())[:-2])
-
-num_classes = 21
-net.add_module("final_conv", nn.Conv2d(512, num_classes, kernel_size=1))
-net.add_module(
-    "transpose_conv",
-    nn.ConvTranspose2d(num_classes, num_classes, kernel_size=64, padding=16, stride=32),
-)
-
-
 def bilinear_kernel(in_channels, out_channels, kernel_size):
+
     factor = (kernel_size + 1) // 2
     if kernel_size % 2 == 1:
         center = factor - 1
@@ -93,44 +104,127 @@ def bilinear_kernel(in_channels, out_channels, kernel_size):
     return weight
 
 
-W = bilinear_kernel(num_classes, num_classes, 64)
-net.transpose_conv.weight.data.copy_(W)
+def build_model(num_classes=21):
 
-batch_size, crop_size = 32, (320, 480)
-train_iter, test_iter = d2l.load_data_voc(batch_size, crop_size)
+    pretrained_net = torchvision.models.resnet18(pretrained=True)
+
+    net = nn.Sequential(*list(pretrained_net.children())[:-2])
+
+    net.add_module("final_conv", nn.Conv2d(512, num_classes, kernel_size=1))
+    net.add_module("transpose_conv", nn.ConvTranspose2d(
+        num_classes, num_classes, kernel_size=64, padding=16, stride=32))
+
+    W = bilinear_kernel(num_classes, num_classes, 64)
+    net.transpose_conv.weight.data.copy_(W)
+
+    return net
 
 
-def loss(inputs, targets):
+def calc_loss(inputs, targets):
     return F.cross_entropy(inputs, targets, reduction="none").mean(1).mean(1)
 
 
-num_epochs, lr, wd, devices = 5, 0.001, 1e-3, d2l.try_all_gpus()
-trainer = torch.optim.SGD(net.parameters(), lr=lr, weight_decay=wd)
-d2l.train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs, devices)
+def predict(net, test_iter, img, *, env):
 
-
-def predict(img):
     X = test_iter.dataset.normalize_image(img).unsqueeze(0)
-    pred = net(X.to(devices[0])).argmax(dim=1)
+    pred = net(X.to(env.device)).argmax(dim=1)
     return pred.reshape(pred.shape[1], pred.shape[2])
 
 
-def label2image(pred):
-    colormap = torch.tensor(d2l.VOC_COLORMAP, device=devices[0])
+def label2image(pred, *, env):
+
+    colormap = torch.tensor(d2l.VOC_COLORMAP, device=env.device)
     X = pred.long()
     return colormap[X, :]
 
+def train(net, train_iter, optimizer, *, env):
 
-voc_dir = d2l.download_extract("voc2012", "VOCdevkit/VOC2012")
-test_images, test_labels = d2l.read_voc_images(voc_dir, False)
-n, imgs = 4, []
-for i in range(n):
-    crop_rect = (0, 0, 320, 480)
-    X = torchvision.transforms.functional.crop(test_images[i], *crop_rect)
-    pred = label2image(predict(X))
-    imgs += [
-        X.permute(1, 2, 0),
-        pred.cpu(),
-        torchvision.transforms.functional.crop(test_labels[i], *crop_rect).permute(1, 2, 0),
-    ]
-d2l.show_images(imgs[::3] + imgs[1::3] + imgs[2::3], 3, n, scale=2)
+    timer = time.perf_counter()
+
+    # animator = d2l.Animator(xlabel='epoch', xlim=[1, epochs], legend=['pixelwise loss'])
+
+    epochs = env.args.epochs
+
+    device = env.device
+    net.to(device)
+
+    losses = []
+    for epoch in range(epochs):
+        print(f'\nepoch: {epoch+1} of {epochs}')
+
+        net.train()
+        for (features, target) in tqdm(train_iter):
+
+            optimizer.zero_grad()
+            X, Y = features.to(device), target.to(device)
+
+            fx = net(X)
+            loss = calc_loss(fx, Y)
+            loss.mean().backward()
+            optimizer.step()
+
+            losses.append(loss.mean())
+
+            # animator.add(epoch + 1, (loss.mean()))
+
+            # save it ... after batch cuz it takes a while
+            if env.args.save:
+                torch.save(net.state_dict(), env.file)
+
+        print(f'  pixelwise loss: {loss.mean()}')
+
+    timer = int(time.perf_counter() - timer)
+    print(f'Finished in {timer} seconds')
+    print(f'{len(train_iter.dataset) / timer:.1f} examples/sec on {str(device)}')
+
+
+def main():
+
+    args = get_args()
+
+    env = Environment()
+    env.file = __file__.split('/')[-1].split('.')[0] + '.pt'
+    env.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    env.args = args
+
+    # define maps
+    VOC_COLORMAP, VOC_CLASSES = get_voc_maps()
+
+    # batch data for training
+    print('getting training data...')
+    batch_size, crop_size = 32, (320, 480)
+    train_iter, test_iter = d2l.load_data_voc(batch_size, crop_size)
+
+    # create a model
+    net = build_model()
+    if args.load:
+        net.load_state_dict(torch.load(env.file))
+    if args.verbose:
+        summary(net, (3,*crop_size))
+
+    # train
+    lr, wd = 0.001, 1e-3
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr, weight_decay=wd)
+
+    if args.train:
+        train(net, train_iter, optimizer, env=env)
+
+    # do some predictions
+    voc_dir = d2l.download_extract("voc2012", "VOCdevkit/VOC2012")
+    test_images, test_labels = d2l.read_voc_images(voc_dir, False)
+
+    print('evaluation')
+    n, imgs = 4, []
+    for i in tqdm(range(n)):
+        crop_rect = (0, 0, 320, 480)
+        crop = lambda imgs: torchvision.transforms.functional.crop(imgs, *crop_rect)
+
+        X = crop(test_images[i])
+        pred = label2image(predict(net, test_iter, X, env=env), env=env)
+        imgs += [X.permute(1, 2, 0), pred.cpu(), crop(test_labels[i]).permute(1, 2, 0)]
+
+    d2l.show_images(imgs[::3] + imgs[1::3] + imgs[2::3], 3, n, scale=2)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
